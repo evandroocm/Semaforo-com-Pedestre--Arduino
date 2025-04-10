e38e4d429fec0711022ba6347ec57c0f77e7beee
Evandro Marques <evandrocastromarques@gmail.com>
2025-04-10 07:15:11 -0300

    update codigo semaforo

--- arduino.ino
+++ arduino.ino
@@ -1,47 +1,57 @@
-#define vmA 13
-#define amA 12
-#define vdA 11
-#define vmpdA 10
-#define vdpdA 9
-#define vmB 8
-#define amB 7
-#define vdB 6
-#define vmpdB 5
-#define vdpdB 4
+// Semáforo A (Carros)
+const int vermelhoCarroA = 13;
+const int amareloCarroA = 12;
+const int verdeCarroA = 11;
 
-void setup()
-{
-  pinMode(vmA, OUTPUT);
-  pinMode(amA, OUTPUT);
-  pinMode(vdA, OUTPUT);
-  pinMode(vmpdA, OUTPUT);
-  pinMode(vdpdA, OUTPUT);
-  pinMode(vmB, OUTPUT);
-  pinMode(amB, OUTPUT);
-  pinMode(vdB, OUTPUT); 
-  pinMode(vmpdB, OUTPUT); 
-  pinMode(vdpdB, OUTPUT);
+// Semáforo A (Pedestres)
+const int verdePedestreA = 10;
+const int vermelhoPedestreA = 9;
+
+// Semáforo B (Carros)
+const int vermelhoCarroB = 8;
+const int amareloCarroB = 7;
+const int verdeCarroB = 6;
+
+// Semáforo B (Pedestres)
+const int verdePedestreB = 5;
+const int vermelhoPedestreB = 4;
+
+void setup() {
+  // Configuração dos pinos como saída
+  pinMode(vermelhoCarroA, OUTPUT);
+  pinMode(amareloCarroA, OUTPUT);
+  pinMode(verdeCarroA, OUTPUT);
+  pinMode(verdePedestreA, OUTPUT);
+  pinMode(vermelhoPedestreA, OUTPUT);
+
+  pinMode(vermelhoCarroB, OUTPUT);
+  pinMode(amareloCarroB, OUTPUT);
+  pinMode(verdeCarroB, OUTPUT);
+  pinMode(verdePedestreB, OUTPUT);
+  pinMode(vermelhoPedestreB, OUTPUT);
 }
-  
-  
-void loop()
-{	 //vm am vd vm vd vm am vd vm vd
-  leds(1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 4);//via um
-  leds(1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 2);//primeiro intervalo para amarelo
-  leds(0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 4);//via dois
-  leds(0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 2);//segundo intervalo para amarelo
-} 
 
-void leds (bool e1, bool e2, bool e3, bool e4, bool e5, bool e6, bool e7, bool e8, bool e9, bool e10, int tempo)
-{
-  digitalWrite(vmA, e1);
-  digitalWrite(amA, e2);
-  digitalWrite(vdA, e3);
-  digitalWrite(vmpdA, e4);
-  digitalWrite(vdpdA, e5);
-  digitalWrite(vmB, e6);
-  digitalWrite(amB, e7);
-  digitalWrite(vdB, e8);
-  digitalWrite(vmpdB, e9);
-  digitalWrite(vdpdB, e10);
-  delay(tempo * 1000);}
+void loop() {
+  // Sequência do semáforo
+  leds(1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 4); // Via A liberada
+  leds(1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 2); // Amarelo B
+  leds(0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 4); // Via B liberada
+  leds(0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 2); // Amarelo A
+}
+
+// Função que ativa os LEDs
+void leds(bool rA, bool aA, bool vA, bool vpA, bool rpA, bool rB, bool aB, bool vB, bool vpB, bool rpB, int tempo) {
+  digitalWrite(vermelhoCarroA, rA);
+  digitalWrite(amareloCarroA, aA);
+  digitalWrite(verdeCarroA, vA);
+  digitalWrite(verdePedestreA, vpA);
+  digitalWrite(vermelhoPedestreA, rpA);
+
+  digitalWrite(vermelhoCarroB, rB);
+  digitalWrite(amareloCarroB, aB);
+  digitalWrite(verdeCarroB, vB);
+  digitalWrite(verdePedestreB, vpB);
+  digitalWrite(vermelhoPedestreB, rpB);
+
+  delay(tempo * 1000);
+}
